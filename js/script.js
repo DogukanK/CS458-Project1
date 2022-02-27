7e877844a818413cb6a6e790e3ad62f9499b4d9c
Doğukan Kurnaz <dogukanertunga@gmail.com>
2022-02-27 20:59:40 +0300

    facebook login implemented

--- js/script.js
+++ js/script.js
@@ -79,3 +79,61 @@ function isUserRegistered() {
             document.location.href = '/mainPage.html';
     }
 }
+let is_form_checked = false;
+document.addEventListener('submit', e => {
+    if(!is_form_checked) {
+      e.preventDefault();
+
+      isUserRegistered();
+      is_form_checked = true;
+    }
+  });
+
+  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+  
+  
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+    successLogin();
+  }
+  
+  
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '473397624421876',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : 'v13.0'           // Use this Graph API version for this call.
+    });
+  
+  
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+  
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+    fbName = response.name;
+
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+  function successLogin() {
+    document.location.href = '/mainPage.html'  
+    }
